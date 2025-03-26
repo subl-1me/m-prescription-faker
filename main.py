903a0b19111f85723c763a7441a05980fffe5ca5
subl-1me <julio.ugalde404@gmail.com>
2025-03-25 19:08:47 -0600

    Improves to main menu selections by adding Action class

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-from controllers.menu import init_home_menu, ask_for_prop, askForProfile
-from const import CREATE_PRESCRIPTION_OPT, MODIFY_MEDICAL_PROFILE_OPT, SHOW_MEDICAL_PROFILE_OPT, SET_MEDICAL_PROFILE
+from controllers.menu import init_home_menu
+from const import  CREATE_PRESCRIPTION_OPT, SHOW_MEDICAL_PROFILE_OPT, MODIFY_MEDICAL_PROFILE_OPT, SET_MEDICAL_PROFILE
 from controllers.actions import Actions
 
 def main():
-    profile = None
     actions = Actions()
-
-    menu_actions = {
-        1: CREATE_PRESCRIPTION_OPT,
-        2: SET_MEDICAL_PROFILE,
-        3: MODIFY_MEDICAL_PROFILE_OPT,
-        4: SHOW_MEDICAL_PROFILE_OPT
-    }
 
     while True:
         selection = init_home_menu()
         if selection == -1:
             continue
-        
-        if selection == 1:
-            menu_actions[selection]() # Create medical document (.word)
-        elif selection == 2:
-            profile = menu_actions[selection]() # Set medical profile
-        elif selection == 3:
-            menu_actions[selection](profile)
-        elif selection == 4: 
-            menu_actions[selection](profile)
+
+        # transform selection to func name
+        parsedSelection = str.lower(selection).replace(' ', '_')
+        if selection == CREATE_PRESCRIPTION_OPT:
+            getattr(actions, parsedSelection)() # Create medical document (.word)
+        elif selection == SET_MEDICAL_PROFILE:
+            getattr(actions, parsedSelection)() # Set medical profile
+        elif selection == MODIFY_MEDICAL_PROFILE_OPT:
+            getattr(actions, parsedSelection)()
+        elif selection == SHOW_MEDICAL_PROFILE_OPT: 
+            getattr(actions, parsedSelection)()
         else:
-            print(f'Invalid option.')
-
-
+            print(f'\nInvalid option. Try again\n')
 
 
 if __name__ == '__main__':
